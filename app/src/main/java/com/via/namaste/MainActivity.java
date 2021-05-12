5637cc79067bd5c8129aa4b1abd94e205f53f0e1
Bogdan Novac <293134@via.dk>
2021-05-12 20:54:04 +0200

    POINT

--- app/src/main/java/com/via/namaste/MainActivity.java
+++ app/src/main/java/com/via/namaste/MainActivity.java
@@ -1,14 +1,101 @@
 package com.via.namaste;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import static com.via.namaste.R.drawable.ic_action_name;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Initialize variable
+    MeowBottomNavigation bottomNavigation;
+
+
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // load the home fragment when the app starts
+//        getSupportFragmentManager().beginTransaction().replace(R.layout.fragment_home, new HomeFragment()).commit();
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        //Add menu item
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic_baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_baseline_add_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_action_name));
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener()
+
+
+
+        {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                //Initialize fragment
+                Fragment fragment = null;
+                //Check condition
+
+                switch (item.getId()) {
+                    case 1:
+                        //When id is 1
+                        // Initialize home fragment
+                        fragment = new HomeFragment();
+                        break;
+
+                    case 2:
+                        //When id is 2
+                        // Initialize add fragment
+                        fragment = new AddFragment();
+                        break;
+                    case 3:
+                        //When id is 3
+                        // Initialize notifications fragment
+                        fragment = new ProgressFragment();
+                        break;
+                    default:
+                        break;
+                }
+
+
+                //Load fragment
+                loadFragment(fragment);
+
+            }
+        });
+
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+
+                //Display Toast
+
+
+
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        //Replace fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, fragment)
+                .commit();
+
     }
 }
---
+++ app/src/main/java/com/via/namaste/SignInActivity.java
@@ -0,0 +1,110 @@
+package com.via.namaste;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class SignInActivity extends AppCompatActivity {
+    // Facebook
+    private CallbackManager mCallbackManager;
+    private LoginButton loginButton;
+    private static final String TAG="FacebookAuthentication";
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_page);
+        mAuth=FirebaseAuth.getInstance();
+user=FirebaseAuth.getInstance().getCurrentUser();
+checkIfSignedIn();
+        // Initialize Facebook Login button
+        mCallbackManager = CallbackManager.Factory.create();
+        loginButton = findViewById(R.id.login_button);
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+
+
+
+    }
+    private void checkIfSignedIn() {
+        if (user != null) {
+            goToHome(user);
+        }
+    }
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Facebook", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToHome(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Facebook", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(SignInActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            goToHome(null);
+                        }
+                    }
+                });
+    }
+
+
+    private void goToHome(FirebaseUser user) {
+        if(user!=null){
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+    }
+}
